b9bc5fb200f7573f85681ecaf3207465d809891d
Kenneth Mercer <kmercer5439@gmail.com>
2022-03-21 22:27:44 -0500

    built up api, most tests passing

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,74 @@
 const router = require('express').Router()
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
+const { checkAccountPayload, checkAccountNameUnique, checkAccountId } = require('./accounts-middleware')
+
+
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+
+  try {
+    const accounts = await Account.getAll()
+    res.json(accounts)
+  }
+  
+  catch(error) {
+    next(error)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
+
+  res.json(req.account)
+  next()
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
   // DO YOUR MAGIC
+
+  Account.create(req.body)
+  .then(newAccount => {
+      res.status(201).json(newAccount)
+    } 
+  ).catch(err => {
+    next(err)
+  })
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', checkAccountId, checkAccountPayload, async (req, res, next) => {
   // DO YOUR MAGIC
+
+  const updated = await Account.updateById(req.params.id, req.body)
+
+  try {
+    res.status(200).json(updated)
+  }
+
+  catch(error) {
+    next(error)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
+
+  try {
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
+  }
+
+  catch(error) {
+    next(error)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+
+  res.status(err.status || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
